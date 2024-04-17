1b65f16b4f8e2c3e59597b88c9d944a65c0b26c3
NATAN <natan.oliveira@limeira.sp.gov.br>
2024-04-17 15:12:10 -0300

    Edições nos documentos

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 app:app
+web: gunicorn -w 5 app:app
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+#configuração do login e afins
 class Config(object):
     # ...
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
